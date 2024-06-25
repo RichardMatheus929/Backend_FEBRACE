4d97bbb1509fa4c4d685303b197ad93f7c756d21
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-25 17:09:15 -0300

    organizando repositorio

--- febrace/settings.py
+++ febrace/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!el(7aty7)2ltpnj^hx&@ol34rc5nvro0a&lh(@e==mhc@u3#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
