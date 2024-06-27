6ce8bbf987da7a44599b6faee9b54a12a42e84e0
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-27 12:04:55 -0300

    Adicionando quantidade de escolas premiadas

--- projetos/views.py
+++ projetos/views.py
@@ -14,6 +14,8 @@ class InfoProject(APIView):
         escola = Projeto.objects.filter(id=id_project).first().escola
         name = Projeto.objects.filter(id=id_project).first().nome
 
+        premios_por_escola = len(set(Projeto.objects.filter(escola=escola).values_list('nome',flat=True)))
+
         quantidade_projetos = Projeto.objects.filter(nome__icontains=name).count()
 
         categorias_premiacoes = Projeto.objects.filter(nome__icontains=name).values_list('categoria_premiacao', flat=True)
@@ -24,6 +26,7 @@ class InfoProject(APIView):
 
         info = {
             'escola': escola,
+            'escola_quantidade':premios_por_escola,
             'premios': quantidade_projetos,
             'categorias': " / ".join(categorias_premiacoes),
             'ano': " / ".join(anos)
